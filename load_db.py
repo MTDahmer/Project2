3b824dce6aee50c79f671c85989601a74912b74e
Lizzy Clay <46448543+aeclaman@users.noreply.github.com>
2019-06-11 22:34:04 -0500

    added changes to load_db.py

--- load_db.py
+++ load_db.py
@@ -1,4 +1,5 @@
 import tweepy
+import pymongo
 from pymongo import MongoClient
 import datetime as DT
 
@@ -8,7 +9,8 @@ from config import consumer_key, consumer_secret, access_token, access_token_sec
 def get_all_tweets(screen_name): 
     client = MongoClient()
     db = client.db_twitter
-    tweets = db.Tweets
+    col = "Tweets_"+screen_name
+    tweets = db[col]
 
     # Creating the authentication object
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -23,25 +25,31 @@ def get_all_tweets(screen_name):
     week_ago = today - DT.timedelta(days=7)
 
     alltweets = []
-    new_tweets = api.search(q=screen_name, lang=language, since=week_ago, count=100)
+    new_tweets = api.search(q=screen_name, lang=language, since=week_ago, count=1000)
     alltweets.extend(new_tweets)
+    print("all tweets length:"+str(len(alltweets)))
     oldest = alltweets[-1].id - 1   
     while len(new_tweets) > 0:
-        new_tweets = api.search(q=screen_name, lang=language, since=week_ago, count=100, max_id=oldest)
+        new_tweets = api.search(q=screen_name, lang=language, since=week_ago, count=1000, max_id=oldest)
         alltweets.extend(new_tweets)
         oldest = alltweets[-1].id - 1
 
         for tweet in alltweets: 
-            if (tweet.retweeted):
+            # if (tweet.retweeted):
+            if ( (hasattr(tweet, 'retweeted_status')) ):
                 pass
             else:
                 tweets.insert_one(tweet._json)
                 print("Method execution is finished.")
 
 # The Twitter users who we want to get tweets from
-# name = "@GovBillWeld"
-candidates = ["@realDonaldTrump", "@BernieSanders", "@JoeBiden", "@SenWarren"]
+candidates = ["@realDonaldTrump", "@BernieSanders", "@JoeBiden", "@SenWarren", "@GovBillWeld", "@JohnDelaney", "@KamalaHarris"]
+
 
 for name in candidates:
-    get_all_tweets(name)
+    try:
+        get_all_tweets(name)
+    except pymongo.errors.DuplicateKeyError:
+        print("DuplicateKeyError")
+        continue
 
