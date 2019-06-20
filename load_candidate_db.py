673bdaf63f833d9af021f7c6e6be251d6cd7a1fa
Lizzy Clay <46448543+aeclaman@users.noreply.github.com>
2019-06-19 20:56:37 -0500

    added a new metadata field

--- load_candidate_db.py
+++ load_candidate_db.py
@@ -5,15 +5,18 @@ import time
 import datetime
 
 # Twitter developer API key
-from config import consumer_key, consumer_secret, access_token, access_token_secret, MONGO_HOST, MONGO_PORT, MONGO_DB, MONGO_USER, MONGO_PASS
+from config import consumer_key, consumer_secret, access_token, access_token_secret 
+#comment out this line for local db load
+from config import MONGO_HOST, MONGO_PORT, MONGO_DB, MONGO_USER, MONGO_PASS
 
 
 def get_all_tweets(screen_name):
+    # following code needed to load local db, then comment out lines 17-19
     # client = MongoClient()
+    # db = client.db_twitter_handle
     con = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
     db = con[MONGO_DB]
     db.authenticate(MONGO_USER, MONGO_PASS)
-    # db = client.db_twitter_handle
     col = "Tweets_from_"+screen_name
     tweets = db[col]
 
@@ -38,7 +41,6 @@ def get_all_tweets(screen_name):
     page_count = 0
     for page in tweepy.Cursor(api.user_timeline, screen_name=screen_name, tweet_mode='extended', count=200).pages(100):
         page_count += 1
-        print("     >>>>> now on page ", page_count)
 
         for tweet in page:
             if (tweet.created_at.year == current_year):
@@ -46,20 +48,24 @@ def get_all_tweets(screen_name):
                 tweets.insert_one(tweet._json)
                 # collect stats for this candidate to be added to a metadata collection
                 candidate_name = tweet.user.name
+                followers = tweet.user.followers_count
                 tweet_count += 1
                 tweet_favorites += tweet.favorite_count
                 tweet_retweets += tweet.retweet_count
 
         time.sleep(1)
-        print("    ...%s tweets evaluated so far" % (len(page)))
+        print("    ...%s tweets evaluated on page %s" % (len(page), page_count))
     candidate_meta_dict["candidate"] = candidate_name
     candidate_meta_dict["screenName"] = screen_name
     candidate_meta_dict["retweetAvg"] = tweet_retweets/tweet_count
     candidate_meta_dict["favoriteAvg"] = tweet_favorites/tweet_count
+    candidate_meta_dict["followers"] = followers
+    print("    ...%s actual tweet count included %s retweets and %s favorites and %s followers for %s" % (tweet_count, tweet_retweets, tweet_favorites, followers, candidate_name))
     return candidate_meta_dict
 
 
 def add_to_metadata(candidateDictionary):
+    # the following code needed to load local db, then comment out lines 68-70
     # client = MongoClient()
     # db = client.db_twitter_handle
     con = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
@@ -73,13 +79,10 @@ def add_to_metadata(candidateDictionary):
 
 
 # The Twitter users who we want to get tweets from
-# candidates = ["@realDonaldTrump", "@BernieSanders", "@JoeBiden", "@SenWarren", "@GovBillWeld", "@JohnDelaney", "@KamalaHarris"]
 candidates = ["@MichaelBennet", "@JoeBiden", "@BilldeBlasio", "@CoryBooker", "@GovernorBullock", "@PeteButtigieg",
               "@JulianCastro", "@JohnDelaney", "@TulsiGabbard", "@SenGillibrand", "@MikeGravel", "@KamalaHarris", "@Hickenlooper",
               "@JayInslee", "@amyklobuchar", "@WayneMessam", "@sethmoulton", "@BetoORourke", "@TimRyan", "@BernieSanders", "@ericswalwell",
               "@realDonaldTrump", "@ewarren", "@GovBillWeld", "@marwilliamson", "@AndrewYang"]
-
-#"@POTUS", "@SenWarren",
 
 for name in candidates:
     try:
