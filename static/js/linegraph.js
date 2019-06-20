1728e955ebbd2993d4ba5c05fa4c68814b7a5cb8
MTDahmer <mitchelldahmer@gmail.com>
2019-06-19 23:04:04 -0500

    linechart functional

--- static/js/linegraph.js
+++ static/js/linegraph.js
@@ -1,34 +1,59 @@
-function grabData() {
-d3.json("http://localhost:5000/data/<chosenCandidate>", function(data){
-    dates = [];
-    followerCount = [];
-    for (x in data) {
-        dates.push(data.created_at);
-        followerCount.push(data.followers);
-    }
-    console.log(dates);
-    console.log(followerCount);
 
-});
-};
+function buildLineGraph(chosenCandidate) {
+var url = `/data/${chosenCandidate}`;
+d3.json(url, function(data){
+    console.log(data);
+    likeDic = {};
+    retweetDic = {};
+    likeData = [];
+    retweetData = [];
+    data.result.forEach((tweet)=> {
+      var date1 = Date.parse(tweet['created_at']);
+      console.log(date1);
+      likeDic = {
+        x : date1,
+        y : tweet['favourite_count']
+      };
+      retweetDic = {
+        x : date1,
+        y : tweet['retweet_count']
+      };
+      likeData.push(likeDic);
+      retweetData.push(retweetDic);
+    });
+    console.log(likeData);
+    console.log(retweetData);
+       
+    var finalData = [
+      {
+        key : "Likes" ,
+        values : likeData,
+        color : "1f77b4"
+
+      },
+      {
+        key : "Retweets" ,
+        values : retweetData,
+        color : "d62728"
+      }
+    ];
+
+
 
 nv.addGraph(function() {
     var chart = nv.models.lineChart()
-      .useInteractiveGuideline(true)
-      ;
-  
+    .useInteractiveGuideline(true);       
+
     chart.xAxis
       .axisLabel('Date')
-      .tickFormat(d3.format('f'))
       ;
   
     chart.yAxis
-      .axisLabel('Follower Count')
-      .tickFormat(d3.format('f'))
+      .axisLabel('# of likes/retweets')
       ;
   
-    d3.select('#chart_two')
-      .datum(grabData())
+    d3.select('#chart_two svg')
+      .datum(finalData)
       .transition().duration(500)
       .call(chart)
       ;
@@ -37,3 +62,5 @@ nv.addGraph(function() {
   
     return chart;
   });
+});
+};
