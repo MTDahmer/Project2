7004d58322f7b37d0adbdbeac691c2de3683ee7b
B Farrington <BWayne422@yahoo.com>
2019-06-19 21:30:08 -0500

    pie data

--- static/js/piechart.js
+++ static/js/piechart.js
@@ -1,16 +1,61 @@
+a= []
 function buildCharts(chosenCandidate) {
 var url = `/data/${chosenCandidate}`;
-d3.json(url).then(function(data){
-  a =[]
-  data.forEach((tweet)=>{
+d3.json(url,function(data){
   
-  a.push(tweet)
+  // text = data.result;
+  data.result.forEach((tweet)=>{
+
+    a.push(tweet['text']);
+   
+  });
+
+
+  w = ['Healthcare', 'Jobs', 'Immigration','Taxes', 'Gun', 'Income', 'foreign', 'Climate','hat cat' ]
+  n =[]
+  w.forEach((topic)=>{
+    var x = countwords(topic)
+      // x.filter(function(n){ return x ==null;})
+      console.log(x);
   })
-console.log(a);
-})
 
-};
+    // if (!x){
+    //   x
+
+    // }
+         
+    // return x})
+
+  // var x = countwords(w);
+  
+  // console.log(x);
   
 
+//Extremely/Very important
+// %
+// Healthcare    80
+// The economy    78
+// Immigration    78
+// Way women are treated in U.S. society    74
+// Gun policy    72
+// Taxes    70
+// Foreign affairs    68
+// Way income and wealth are distributed in the U.S.    68
+// The recent confirmation of Brett Kavanaugh to the Supreme Court    64
+// U.S. trade and tariff policies    61
+// Climate change
+});
 
-////-------------------------------------------------------
+
+
+}
+
+function countwords(word){
+
+  // var res = a.join().match(/word/g);
+  var re = new RegExp(word.toLowerCase(), 'g');
+  var res = a.join().match(re);
+  console.log(res);
+  return res.length
+}
+
