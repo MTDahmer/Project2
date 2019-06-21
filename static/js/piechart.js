6c78715b75a0eea07aed228b29273a222a6d1c2b
Lizzy Clay <46448543+aeclaman@users.noreply.github.com>
2019-06-20 20:18:31 -0500

    pie configuration

--- static/js/piechart.js
+++ static/js/piechart.js
@@ -1,39 +1,40 @@
 a= []
 
 function buildCharts(chosenCandidate) {
-var url = `/data/${chosenCandidate}`;
-d3.json(url,function(data){
-  data.result.forEach((tweet)=>{
-    a.push(tweet['text']);
-   });
+  var url = `/data/${chosenCandidate}`;
+  d3.json(url,function(data){
+    data.result.forEach((tweet)=>{
+     a.push(tweet['text']);
+    });
 
    
-   topic_x = [];
- var w = ['Healthcare', 'Jobs', 'Immigration','Taxes', 'Gun', 'Income', 'foreign', 'Climate'];
+    topic_x = [];
+    var w = ['Healthcare', 'Jobs', 'Immigration','Taxes', 'Gun', 'Income', 'foreign', 'Climate'];
     w.forEach((topic)=>{
       hat = countwords(topic);
       topic_x.push(hat);
 
       console.log(hat);
     });
-  
+    
     console.log(topic_x);
     //  console.log(countwords('health'));
 
-  var var_topics = [
-    {key: "Healthcare", y: topic_x[0], color: "#5F5"},
-    {key: "Jobs", y: topic_x[1]},
-    {key: "Immigration", y: topic_x[2]},
-    {key: "Taxes", y: topic_x[3]},
-    {key: "Gun", y: topic_x[4]},
-    {key: "Income", y: topic_x[5]},
-    {key: "Foreign", y: topic_x[6]},
-    {key: "Climate", y: topic_x[7]}];
+    var var_topics = [
+      {key: "Healthcare", y: topic_x[0], color: "#5F5"},
+      {key: "Jobs", y: topic_x[1]},
+      {key: "Immigration", y: topic_x[2]},
+      {key: "Taxes", y: topic_x[3]},
+      {key: "Gun", y: topic_x[4]},
+      {key: "Income", y: topic_x[5]},
+      {key: "Foreign", y: topic_x[6]},
+      {key: "Climate", y: topic_x[7]}
+    ];
 
     console.log(var_topics);
 
-    var height = 200;
-    var width = 200;
+    var height = 400;
+    var width = 400;
 
     nv.addGraph(function() {
       var chart = nv.models.pieChart()
@@ -41,10 +42,10 @@ d3.json(url,function(data){
           .y(function(d) { return d.y })
           .showLabels(true)
           .width(width)
-          .height(height);
-          // .growOnHover(false)
-          // .labelType('key')
-          // .showTooltipPercent(true);
+          .height(height)
+          .growOnHover(false)
+          .labelType('key')
+          .showTooltipPercent(true);
 
         d3.select("#chart_one svg")
           .datum(var_topics)
@@ -57,11 +58,12 @@ d3.json(url,function(data){
         //   var_topics[1].y = Math.floor(Math.random() * 10);
         //   chart.update();
         // }, 4000);
-        return;});
-
+        return chart;
       });
 
-      }
+    });
+
+  }
 
 function countwords(word){
   // var res = a.join().match(/word/g);
@@ -71,6 +73,3 @@ function countwords(word){
   console.log (res.length)
   return res.length};
   
-  
-
-
